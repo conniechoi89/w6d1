063a884fd5059c7ea6da9891cfd96e773674e756
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-12-05 01:41:02 +0000

    New ME!

--- class_Invoice.rb
+++ class_Invoice.rb
@@ -4,13 +4,36 @@ class Person
   #   @name = name
   # end
 
-  attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
+  attr_reader :name
+  attr_writer :name
+
 end
 
 p = Person.new('L. Ron')
+p.name = 'NewME!'
 puts p.name
+
+
+
+
+# class Person
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def name=(value)
+#     @name = value
+#     @name = New Name
+#   end
+
+# end
